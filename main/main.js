17c7d7fc88fa953c310f0a34863b00fd5e76deb7
xiallyspider <33478703+xiallyspider@users.noreply.github.com>
2018-04-17 10:10:53 +0800

    完成逻辑

--- main/main.js
+++ main/main.js
@@ -1,4 +1,32 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+// 0<里程数<=2
+function withInTwoKilo(mileage, time){
+    let result = 0
+    result = (6 + 0.25*time).toFixed(2)
+    return result
+}
+// 2<里程数<8
+function twoToEight(mileage, time){
+    let result = 0
+    result = (6 + 0.8*mileage + 0.25*time).toFixed(2)
+    return result
+}
+// 8<=里程数
+function eightOrMore(mileage, time){
+    let result = 0
+    result = (6 +0.8*(1 + 0.5)*mileage + 0.25*time).toFixed(2)
+    return result
+}
+module.exports = function main(mileage, time) {
+    let result = 0
+    if (0 < mileage && mileage <= 2) {
+        result = withInTwoKilo(mileage, time)
+    }
+    if (2 < mileage && mileage < 8) {
+        result = twoToEight(mileage, time)
+    }
+    if (8 <= mileage) {
+        result = eightOrMore(mileage, time)
+    }
+    result = Math.round(result)
+    return result
 };
